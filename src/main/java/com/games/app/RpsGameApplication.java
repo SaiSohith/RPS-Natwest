886087976af1e27fd5b555cf7691b76d619c6058
SaiSohith <saisohith9678@gmail.com>
2022-12-20 17:41:13 +0530

    Cors Origin Error Correction

--- src/main/java/com/games/app/RpsGameApplication.java
+++ src/main/java/com/games/app/RpsGameApplication.java
@@ -2,6 +2,7 @@ package com.games.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -13,6 +14,7 @@ public class RpsGameApplication {
 		SpringApplication.run(RpsGameApplication.class, args);
 	}
 	
+	@CrossOrigin
 	@GetMapping(path="/")
 	public String getValue() {
 		return "index.html";
--- src/main/java/com/games/app/controller/ActionController.java
+++ src/main/java/com/games/app/controller/ActionController.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import com.games.app.ApiResponse;
 import com.games.app.service.ActionService;
 
 @RestController
+@CrossOrigin
 
 @RequestMapping(path="action/")
 
